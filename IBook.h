e3aae490049342e83047ea1e2352ad12a0dbf5b3
KoniChivaJS <mredik200@gmail.com>
2024-03-15 11:31:21 +0200

    Initialize IBook class interface

---
+++ IBook.h
@@ -0,0 +1,17 @@
+//
+// Created by eduard on 15.03.24.
+//
+
+#ifndef LAB2_1_IBOOK_H
+#define LAB2_1_IBOOK_H
+#include <iostream>
+#include <string>
+using namespace std;
+class IBook{
+public:
+    string virtual getName() const = 0;
+    string virtual getAuthor() const = 0;
+    int virtual getPrice() const = 0;
+    
+};
+#endif //LAB2_1_IBOOK_H
