785571f47eebc7b522bb4dc10aa77c5f66336944
KoniChivaJS <mredik200@gmail.com>
2024-03-26 10:41:59 +0200

    Add User options in main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "OwnRecipeBook.h"
 #include "PopularRecipeBooks.h"
 #include <string>
+#include "User.h"
 using namespace std;
 
 void makeNewDish(OwnRecipeBook &HowToCook){
@@ -39,9 +40,49 @@ void changeDescription(OwnRecipeBook& HowToCook){
     HowToCook.setAuthor(newAuthor);
     HowToCook.setPrice(price);
 }
-
 void showBookName(RecipeBook &book){
     cout<<"Welcome to: "<<book.getName()<<endl;
+}
+
+bool userMethods(OwnRecipeBook &HowToCook,PopularRecipeBooks &allRecipes, OwnRecipeBook &CopyBook){
+    int choice = 0;
+    showBookName(HowToCook);
+    cout<<"1: Get dishes";
+    allRecipes.isRecipesBought() ? cout<<"\n2: Show Popular Recipes" : cout<<"\n3: Buy Popular Recipe Books";
+    cout<<"\n3: Make Copy RecipeBook"
+          "\n4: Show Copy RecipeBook"
+          "\n5: Compare RecipeBooks"
+          "\n6: Show info about RecipeBook"
+          "\n7: Exit"
+          "\nChoose option: ";
+    cin>>choice;
+
+    switch (choice) {
+        case 1:
+            HowToCook.getDishes();
+            break;
+        case 2:
+            allRecipes.isRecipesBought() ? allRecipes.getBooks() : allRecipes.actionBuyBook();
+            break;
+        case 3:
+            CopyBook = HowToCook;
+            break;
+        case 4:
+            CopyBook.getDishes();
+            break;
+        case 5:
+            CopyBook == HowToCook ? cout<<"The Books are Similar"<<endl : cout<<"The Books are Diffetent"<<endl;
+            break;
+        case 6:
+            HowToCook.showInfo();
+            break;
+        case 7:
+            return false;
+        default:
+            cout<<"Wrong choice!"<<endl;
+            break;
+    }
+    return true;
 }
 
 int main() {
@@ -50,54 +91,12 @@ int main() {
     PopularRecipeBooks allRecipes("100BooksList",99,false,
                                                     {"La Pizzas - `link`","Baguettes - `link`","National Dishes - `link`","100 Types of pasta - `link`"},
                                                     "Aleg Tenet");
+    User user("konichiva","oleg","konichiva@gmail.com",false);
+
     bool whileWorking = true;
     while (whileWorking){
-        int choice = 0;
-        showBookName(HowToCook);
-        cout<<"1: Make New Dish"
-              "\n2: Get dishes";
-        allRecipes.isRecipesBought() ? cout<<"\n3: Show Popular Recipes" : cout<<"\n3: Buy Popular Recipe Books";
-        cout<<"\n4: Change Description of own book"
-              "\n5: Make Copy RecipeBook"
-              "\n6: Show Copy RecipeBook"
-              "\n7: Compare RecipeBooks"
-              "\n8: Show info about ur own recipe book"
-              "\n9: Exit"
-              "\nChoose option: ";
-        cin>>choice;
+        whileWorking = userMethods(HowToCook,allRecipes,CopyBook);
 
-        switch (choice) {
-            case 1:
-                makeNewDish(HowToCook);
-                break;
-            case 2:
-                HowToCook.getDishes();
-                break;
-            case 3:
-                allRecipes.isRecipesBought() ? allRecipes.getBooks() : allRecipes.actionBuyBook();
-                break;
-            case 4:
-                changeDescription(HowToCook);
-                break;
-            case 5:
-                CopyBook = HowToCook;
-                break;
-            case 6:
-                CopyBook.getDishes();
-                break;
-            case 7:
-                CopyBook == HowToCook ? cout<<"The Books are Similar"<<endl : cout<<"The Books are Diffetent"<<endl;
-                break;
-            case 8:
-                HowToCook.showInfo();
-                break;
-            case 9:
-                whileWorking = false;
-                break;
-            default:
-                cout<<"Wrong choice!"<<endl;
-                break;
-        }
     }
 
     return 0;
