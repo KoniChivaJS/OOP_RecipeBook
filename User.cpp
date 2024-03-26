090e4daebcf136cb51ca6c355a3fd6f197f7f5ec
KoniChivaJS <mredik200@gmail.com>
2024-03-26 10:02:58 +0200

    Logic in User

--- User.cpp
+++ User.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "User.h"
+
+User::User() {
+
+}
+
+User::User(string &newLogin, string &newPassword, string &newEmail, bool admin)
+    :login(newLogin),password(newPassword),email(newEmail),isAdmin(admin){
+
+}
+
+User::~User() {
+
+}
--- User.h
+++ User.h
@@ -5,9 +5,19 @@
 #ifndef LAB2_1_USER_H
 #define LAB2_1_USER_H
 
-
+#include <iostream>
+#include <string>
+using namespace std;
 class User {
-    
+private:
+    string login;
+    string password;
+    string email;
+    bool isAdmin;
+public:
+    User();
+    User(string &newLogin,string &newPassword,string &newEmail,bool admin);
+    ~User();
 };
 
 
