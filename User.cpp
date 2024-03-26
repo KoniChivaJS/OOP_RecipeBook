63274c30f0ca90340d05bb47254020a8bacc6bec
KoniChivaJS <mredik200@gmail.com>
2024-03-26 09:57:42 +0200

    Initialize User class

---
+++ User.cpp
@@ -0,0 +1,5 @@
+//
+// Created by eduard on 26.03.24.
+//
+
+#include "User.h"
---
+++ User.h
@@ -0,0 +1,14 @@
+//
+// Created by eduard on 26.03.24.
+//
+
+#ifndef LAB2_1_USER_H
+#define LAB2_1_USER_H
+
+
+class User {
+    
+};
+
+
+#endif //LAB2_1_USER_H
